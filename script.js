94d0142c1077297320a167113d22b4ee0279bcd9
Josemaria Ofurum <Josemariaofurum@gmail.com>
2024-07-24 08:22:49 +0100

    with snap properties in css, to touch event needed

--- script.js
+++ script.js
@@ -26,24 +26,3 @@ slider.addEventListener('mousemove', (e) => {
   diff = pos - e.clientX ;
   e.currentTarget.scrollLeft += diff;
 })
-
-// touch events
-slider.addEventListener('touchstart', (e) => {
-dragging = true;
-console.log(e);
-pos = e.touches[0].clientX;
-e.currentTarget.style.cursor = 'grabbing';
-})
-
-slider.addEventListener('touchend', (e) => {
-  dragging = false;
-  e.currentTarget.style.cursor = 'grab';
-})
-
-slider.addEventListener('touchmove', (e) => {
-  if (!dragging) return;
-  diff = pos - e.touches[0].clientX  ||  e.clientX ;
-  e.currentTarget.scrollLeft += diff;
-})
-
-
